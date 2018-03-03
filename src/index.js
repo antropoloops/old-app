466bbba2ced2c20e023911f0e1a7803b24cefe19
danigb <danigb@gmail.com>
2018-03-03 18:43:28 +0100

    Use react-scripts

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
-const d3 = require("d3");
+import * as d3 from "d3";
 
 d3
   .select("body")
   .append("div")
   .html("Hola");
+
+d3
+  .queue()
+  .defer(d3.json, "110m.json")
+  .await(() => {
+    console.log("data!");
+  });
+
+d3
+  .queue()
+  .defer(d3.json, "continentes.aset.json")
+  .await((err, set) => {
+    console.log(set);
+  });
