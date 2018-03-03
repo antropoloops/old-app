7872efdaec3439f4b329d155b817429c247f8bc6
Esperanza Moreno Cruz <hola@mi-mina.com>
2018-03-03 19:32:53 +0100

    wip

---
+++ src/visuals/render-map.js
@@ -0,0 +1,49 @@
+import * as d3 from "d3";
+import * as d3geo from "d3-geo-projection";
+import * as topojson from "topojson";
+
+export default function displayData(geoData, set) {
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  const mapDiv = d3.select("body");
+  const svg = mapDiv
+    .append("svg")
+    .attr("class", "svgMap")
+    .attr("width", width)
+    .attr("height", height);
+  const g = svg.append("g");
+
+  d3.select(".svgMap").on("click", (d, i, node) => {
+    const el = node[i];
+    const rfs =
+      el.requestFullscreen ||
+      el.webkitRequestFullScreen ||
+      el.mozRequestFullScreen ||
+      el.msRequestFullscreen;
+
+    rfs.call(el);
+  });
+  const projection = d3geo
+    .geoRobinson()
+    .scale(200)
+    .translate([width / 2, height / 2]);
+  const path = d3.geoPath().projection(projection);
+  const countriesGeometries = topojson.feature(
+    geoData,
+    geoData.objects.countries
+  ).features;
+  const drawMap = (container, data) => {
+    container
+      .selectAll(".countries")
+      .data(data)
+      .enter()
+      .append("path")
+      .attr("class", "countries")
+      .attr("d", path)
+      .style("stroke", "#2c2c2c")
+      .style("stroke-width", 0.5)
+      .style("fill", "#888888");
+  };
+  drawMap(g, countriesGeometries);
+  // window.onresize = drawMap(g, countriesGeometries)
+}
