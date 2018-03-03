c96613b31fa59f6138ba03b913bc3b57180cc236
danigb <danigb@gmail.com>
2018-03-03 19:57:46 +0100

    Events

--- src/index.js
+++ src/index.js
@@ -4,5 +4,6 @@ import renderMap from "./visuals/render-map";
 const loadJson = path => fetch(path).then(response => response.json());
 
 loadJson("antropoloops.set.json").then(set => {
-  loadJson(set.background.geoData).then(geoData => renderMap(geoData, set));
+  const el = document.body;
+  loadJson(set.background.geoData).then(geoData => renderMap(el, geoData, set));
 });
