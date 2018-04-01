426b29faa1334aefdfb5ddc6c2a5ae3d1e61386b
danigb <danigb@gmail.com>
2018-04-01 23:39:26 +0200

    Remove unused code

--- modules/server/src/app.js
+++ modules/server/src/app.js
@@ -1,13 +1,14 @@
 const path = require("path");
 const express = require("express");
-// const favicon = require("serve-favicon");
 const compress = require("compression");
-const cors = require("cors");
-const logger = require("winston");
+// const favicon = require("serve-favicon");
+// const cors = require("cors");
+// const logger = require("winston");
 
 const app = express();
 app.use(compress());
 app.use("/data", express.static(path.join(__dirname, "../../../data")));
 app.use("/public", express.static(path.join(__dirname, "../public")));
+app.use("/app", express.static(path.join(__dirname, "../../webapp/build")));
 
 module.exports = app;
--- modules/server/src/index.js
+++ modules/server/src/index.js
@@ -7,24 +7,12 @@ const ip = require("ip");
 const port = config.get("port");
 const server = app.listen(port);
 
-console.log("IP", ip.address());
-const oscServer = require("./osc");
-
-oscServer.open();
-
-oscServer.on("open", () => {
-  console.log("OSC server started");
-  console.log("OSC", oscServer, Object.keys(oscServer));
-});
-
 process.on("unhandledRejection", (reason, p) =>
   logger.error("Unhandled Rejection at: Promise ", p, reason)
 );
 
-server.on("listening", () =>
-  logger.info(
-    "Antropoloops server started on http://%s:%d",
-    app.get("host"),
-    port
-  )
-);
+server.on("listening", () => {
+  const address = "http://" + ip.address() + ":" + port;
+  logger.info("Antropoloops server started on %s", address);
+  logger.info("Open app %s/app ", address);
+});
